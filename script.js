1c6ff505ec164ee9dc84275c58936fc1b0706f7d
Waz <kilgor3@gmail.com>
2023-03-20 12:26:12 -0400

    Add very basic JS functionality

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+let numA = Number(prompt("Number A?"));
+let numB = Number(prompt("Number B?"));
+let operator = prompt("Operator");
+
+//Operate function
+function operate(a, b, op) {
+  if (op == "+") {
+    return add(a, b);
+  } else if (op == "-") {
+    return subtract(a, b);
+  } else if (op == "*") {
+    return multiply(a, b);
+  } else if (op == "/") {
+    return divide(a, b);
+  }
+}
+console.log(operate(numA, numB, operator));
+
+//Math functions
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
